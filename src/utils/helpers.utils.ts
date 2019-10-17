12a7bf310138989dac85cb17ea77580e06e8a69b
JeDaYoshi <jedayoshi@disroot.org>
2019-10-17 02:13:53 -0400

    Use portalId even if not in development

--- src/utils/helpers.utils.ts
+++ src/utils/helpers.utils.ts
@@ -2,7 +2,7 @@ import os from 'os'
 import { argv } from 'yargs'
 
 export const fetchPortalId = () => {
-    if(process.env.NODE_ENV === 'development' && argv.portalId) return argv.portalId as string
+    if(argv.portalId) return argv.portalId as string
 
     return os.hostname().split('-')[1] as string
 }
