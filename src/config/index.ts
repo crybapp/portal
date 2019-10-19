a50c6dc5489f5543663ff24993bb8e8b96556b50
JeDaYoshi <jedayoshi@disroot.org>
2019-10-19 15:36:15 -0400

    This shouldn't be here

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ require('dotenv').config();
 export default {
     width: parseInt(process.env.VIDEO_WIDTH) || 720,
     height: parseInt(process.env.VIDEO_HEIGHT) || 480,
-    videoBitrate: process.env.VIDEO_BITRATE || '900k',
+    videoBitrate: process.env.VIDEO_BITRATE || '1200k',
     videoFps: process.env.VIDEO_FPS || '30',
     audioBitrate: process.env.AUDIO_BITRATE || '128k',
 
