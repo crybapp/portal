ff832770c96759c73667ec86c8dc1809f1d9ad16
JeDaYoshi <hi@jeda.im>
2022-08-21 19:47:42 -0400

    fix

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,4 +1,4 @@
-import processExists from 'process-exists'
+import { processExists } from 'process-exists'
 import { convertKey } from '../utils/keyboard.utils'
 import { xvfb, pulseaudio, openbox, chromium, xdotool, janusVideo, janusAudio, apertureVideo, apertureAudio } from './utils'
 import { signToken } from '../utils/generate.utils'
